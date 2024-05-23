e350e3f680954b4a8e0cfb92aae4552757561e8f
Kevin Mok <kevin.mok@mail.utoronto.ca>
2024-05-23 15:14:00 -0400

    Add PV's

---
+++ apply.sh
@@ -0,0 +1,1 @@
+kubectl apply -f deployment.yaml,pv-config.yaml,pv-db.yaml,pv-images.yaml,pv-tmp.yaml,pvc-config.yaml,pvc-db.yaml,pvc-images.yaml,pvc-tmp.yaml,service.yaml
---
+++ delete.sh
@@ -0,0 +1,1 @@
+kubectl delete -f deployment.yaml,pv-config.yaml,pv-db.yaml,pv-images.yaml,pv-tmp.yaml,pvc-config.yaml,pvc-db.yaml,pvc-images.yaml,pvc-tmp.yaml,service.yaml
